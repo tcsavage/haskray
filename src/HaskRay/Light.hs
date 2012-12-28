faab022e101160843c58d7ce842fc270e7afaea7
tcsavage <tcsavage@gmail.com>
2012-12-28 13:36:15 +0000

    Switching to spaces instead of tabs

--- src/HaskRay/Light.hs
+++ src/HaskRay/Light.hs
@@ -14,60 +14,60 @@ import System.Random
 -- TODO: Shadow through reflection/refraction
 doLighting :: Vec3 -> Vec3 -> Object -> Render Scalar
 doLighting x norm light@(Object ls (Emissive colour empow)) = do
-	obs <- ask
-	eps1 <- getRandomR (0, 1)
-	eps2 <- getRandomR (0, 1)
-	let sphere = fromMaybe (error "Emissive surface not sphere") $ cast ls
-	let (Sphere center radius) = sphere
-	let sw = center `sub` x
-	let su = normalize (if (abs $ x3 sw) > 1 then Vector3 0 1 0 else Vector3 1 0 0)
-	let sv = sw `cross` su
-	let cosAMax = sqrt (1 - radius * radius / ((x `sub` center) `dot` (x `sub` center)))
-	let cosA = 1 - eps1 + (eps1 * cosAMax)
-	let sinA = sqrt $ 1 - cosA * cosA
-	let phi = 2 * pi * eps2
-	let l = normalize $ (scale ((cos phi) * sinA) su) `add` (scale ((sin phi) * sinA) sv) `add` (scale cosA sw)
-	let ray = Ray x l
-	let closestOb = closestIntersectObStruct ray obs
-	--let getOb (Just (_,_,o)) = o
-	--let closestObIsLight = if isNothing closestOb then True else (if (getOb closestOb) == light then True else False)
-	let closestObIsLight = fromMaybe True $ closestOb >>= (\(_,_,o) -> Just $ o == light)
-	let omega = 2*pi*(1-cosAMax)
-	let fact = empow * (l `dot` norm) * omega
-	return $ if closestObIsLight then clamp fact else 0
-	where
-		clamp fac = if fac < 0 then 0 else fac
-		isReflectiveOrTransmissive (_, (Intersection _ _ _ (Reflective)), _) = True
-		isReflectiveOrTransmissive (_, (Intersection _ _ _ (Transmissive _ _)), _) = True
-		isReflectiveOrTransmissive _ = False
+    obs <- ask
+    eps1 <- getRandomR (0, 1)
+    eps2 <- getRandomR (0, 1)
+    let sphere = fromMaybe (error "Emissive surface not sphere") $ cast ls
+    let (Sphere center radius) = sphere
+    let sw = center `sub` x
+    let su = normalize (if (abs $ x3 sw) > 1 then Vector3 0 1 0 else Vector3 1 0 0)
+    let sv = sw `cross` su
+    let cosAMax = sqrt (1 - radius * radius / ((x `sub` center) `dot` (x `sub` center)))
+    let cosA = 1 - eps1 + (eps1 * cosAMax)
+    let sinA = sqrt $ 1 - cosA * cosA
+    let phi = 2 * pi * eps2
+    let l = normalize $ (scale ((cos phi) * sinA) su) `add` (scale ((sin phi) * sinA) sv) `add` (scale cosA sw)
+    let ray = Ray x l
+    let closestOb = closestIntersectObStruct ray obs
+    --let getOb (Just (_,_,o)) = o
+    --let closestObIsLight = if isNothing closestOb then True else (if (getOb closestOb) == light then True else False)
+    let closestObIsLight = fromMaybe True $ closestOb >>= (\(_,_,o) -> Just $ o == light)
+    let omega = 2*pi*(1-cosAMax)
+    let fact = empow * (l `dot` norm) * omega
+    return $ if closestObIsLight then clamp fact else 0
+    where
+        clamp fac = if fac < 0 then 0 else fac
+        isReflectiveOrTransmissive (_, (Intersection _ _ _ (Reflective)), _) = True
+        isReflectiveOrTransmissive (_, (Intersection _ _ _ (Transmissive _ _)), _) = True
+        isReflectiveOrTransmissive _ = False
 
 --getRandomPoint :: StdGen -> Vec3 -> Sphere -> (Vec3, StdGen)
 --getRandomPoint rand x (Sphere center radius) = (l, rand')
---	where
---		sw = center `sub` x
---		su = normalize (if (abs $ x3 sw) > 1 then Vector3 0 1 0 else Vector3 1 0 0)
---		sv = sw `cross` su
---		cosAMax = sqrt (1 - radius * radius / ((x `sub` center) `dot` (x `sub` center)))
---		(eps1, eps2, rand') = get2Rands rand
---		cosA = 1 - eps1 + (eps1 * cosAMax)
---		sinA = sqrt $ 1 - cosA * cosA
---		phi = 2 * pi * eps2
---		l = normalize $ (scale ((cos phi) * sinA) su) `add` (scale ((sin phi) * sinA) sv) `add` (scale cosA sw)
+--  where
+--      sw = center `sub` x
+--      su = normalize (if (abs $ x3 sw) > 1 then Vector3 0 1 0 else Vector3 1 0 0)
+--      sv = sw `cross` su
+--      cosAMax = sqrt (1 - radius * radius / ((x `sub` center) `dot` (x `sub` center)))
+--      (eps1, eps2, rand') = get2Rands rand
+--      cosA = 1 - eps1 + (eps1 * cosAMax)
+--      sinA = sqrt $ 1 - cosA * cosA
+--      phi = 2 * pi * eps2
+--      l = normalize $ (scale ((cos phi) * sinA) su) `add` (scale ((sin phi) * sinA) sv) `add` (scale cosA sw)
 
 --get2Rands :: StdGen -> (Double, Double, StdGen)
 --get2Rands rand = (fst r1, fst r2, snd r2)
---	where
---		r1 = randomR (0,1) rand
---		r2 = randomR (0,1) $ snd r1
+--  where
+--      r1 = randomR (0,1) rand
+--      r2 = randomR (0,1) $ snd r1
 
 --calculateShadow :: Ray -> [Object] -> Object -> Colour
 --calculateShadow ray@(Ray o _) scene light
---	| closestObIsLight = Vector3 1 1 1
---	| otherwise = Vector3 (0) (0) (0)
---	where
---		dist = mag $ (centerOb light) `sub` o
---		hits = catMaybes $ map (intersectOb ray) scene
---		closest = filter (\(d, _) -> d <= dist)
---		closestOb = closestIntersectOb ray scene
---		getOb (Just (_,_,o)) = o
---		closestObIsLight = if isNothing closestOb then True else (if (getOb closestOb) == light then True else False)
+--  | closestObIsLight = Vector3 1 1 1
+--  | otherwise = Vector3 (0) (0) (0)
+--  where
+--      dist = mag $ (centerOb light) `sub` o
+--      hits = catMaybes $ map (intersectOb ray) scene
+--      closest = filter (\(d, _) -> d <= dist)
+--      closestOb = closestIntersectOb ray scene
+--      getOb (Just (_,_,o)) = o
+--      closestObIsLight = if isNothing closestOb then True else (if (getOb closestOb) == light then True else False)
--- src/HaskRay/RayTree.hs
+++ src/HaskRay/RayTree.hs
@@ -40,88 +40,88 @@ data Shadow = Shadow Colour deriving (Show, Eq)
 -- | Builds a ray tree for a pixel.
 tracePixel :: [Ray] -> Render Pixel
 tracePixel rays = do
-	mapM (traceSample) rays >>= (return . Pixel)
+    mapM (traceSample) rays >>= (return . Pixel)
 
 -- | Evaluates the ray tee under a pixel to determine a final 'Colour' value.
 evalPixel :: Pixel -> Colour
 evalPixel (Pixel samples) = scale (1/(fromIntegral $ length samples)) $ foldr add (Vector3 0 0 0) sampleColours
-	where
-		sampleColours = map (clampColour . evalSample) samples
-		correct col = scale (0.25) (clampColour col)
+    where
+        sampleColours = map (clampColour . evalSample) samples
+        correct col = scale (0.25) (clampColour col)
 
 traceSample :: Ray -> Render Sample
 traceSample ray = do
-	obs <- ask
-	let os = getObjects obs ray
-	procIntersection $ closestIntersectObStruct ray obs
-	where
-		procIntersection Nothing = return Background
-		procIntersection (Just (_, i@(Intersection norm point ray material), ob)) = do
-			obs <- ask
-			procMaterial obs material i ob
-		procMaterial obs (Diffuse col) i _ = do
-			obs <- ask
-			light <- mapM (traceLight i) $ lights obs
-			return $ Diff col light Dead
-		procMaterial obs (Emissive col _) i _ = return $ Emm col
-		procMaterial obs (Reflective) i _ = traceReflection i
-		procMaterial obs (Transmissive _ _) i ob = traceTransmission i ob
-		lights obs = filter isEmissive (getObjects obs ray)
-		notLight (_, (Intersection _ _ _ m)) = case m of
-			(Emissive _ _) -> False
-			otherwise -> True
+    obs <- ask
+    let os = getObjects obs ray
+    procIntersection $ closestIntersectObStruct ray obs
+    where
+        procIntersection Nothing = return Background
+        procIntersection (Just (_, i@(Intersection norm point ray material), ob)) = do
+            obs <- ask
+            procMaterial obs material i ob
+        procMaterial obs (Diffuse col) i _ = do
+            obs <- ask
+            light <- mapM (traceLight i) $ lights obs
+            return $ Diff col light Dead
+        procMaterial obs (Emissive col _) i _ = return $ Emm col
+        procMaterial obs (Reflective) i _ = traceReflection i
+        procMaterial obs (Transmissive _ _) i ob = traceTransmission i ob
+        lights obs = filter isEmissive (getObjects obs ray)
+        notLight (_, (Intersection _ _ _ m)) = case m of
+            (Emissive _ _) -> False
+            otherwise -> True
 
 evalSample :: Sample -> Colour
 evalSample Background = Vector3 0 0 0
 evalSample Dead = Vector3 1 1 1
 evalSample (Diff col shadows _) = foldr (add . shadCol) (Vector3 0 0 0) shadows
-	where shadCol (Shadow scol) = scale (1/pi) $ col `multColour` scol
+    where shadCol (Shadow scol) = scale (1/pi) $ col `multColour` scol
 evalSample (Emm col) = col
 evalSample (Reflection sample) = evalSample sample
 evalSample (Refraction trans ref mix) = t `add` r
-	where
-		amix = max 0 (min 1 mix)
-		omix = 1-amix
-		t = scale amix $ evalSample trans
-		r = scale omix $ evalSample ref
+    where
+        amix = max 0 (min 1 mix)
+        omix = 1-amix
+        t = scale amix $ evalSample trans
+        r = scale omix $ evalSample ref
 
 traceLight :: Intersection -> Object -> Render Shadow
 traceLight (Intersection norm point (Ray origin dir) (Diffuse colour)) lo@(Object light matfun) = do
-	obs <- ask
-	fact <- doLighting point norm lo
-	return $ Shadow $ scale fact colour
+    obs <- ask
+    fact <- doLighting point norm lo
+    return $ Shadow $ scale fact colour
 
 traceReflection :: Intersection -> Render Sample
 traceReflection (Intersection norm point (Ray _ dir) _) = do
-	obs <- ask
-	traceSample ray >>= (return . Reflection)
-	where
-		r = dir `sub` (scale (2 * (norm `dot` dir)) norm)
-		ray = Ray point r
+    obs <- ask
+    traceSample ray >>= (return . Reflection)
+    where
+        r = dir `sub` (scale (2 * (norm `dot` dir)) norm)
+        ray = Ray point r
 
 -- Trace refraction at entry only.
 -- TODO: fresnel reflection
 traceTransmission :: Intersection -> Object -> Render Sample
 traceTransmission int@(Intersection norm point (Ray _ dir) mat@(Transmissive i m)) ob = do
-	obs <- ask
-	samp <- maybe (return Background) traceSample mray2
-	ref <- traceReflection int
-	return $ Refraction samp ref m
-	where
-		refract nout nin norm point dir = normalize $ (scale (nout/nin) (scale (dir `dot` norm) norm) `add` dir) `sub` (scale (sqrt (1-((nout^^2)*(1-(dir `dot` norm)^^2)/(nin^^2)))) norm)
-		r1 = refract 1.0 i norm point dir
-		ray1 = Ray point r1
-		mray2 = do
-			(_, (Intersection norm point (Ray _ dir) (Transmissive i _))) <- intersectOb ray1 ob
-			return $ Ray point $ refract i 1.0 norm point dir
+    obs <- ask
+    samp <- maybe (return Background) traceSample mray2
+    ref <- traceReflection int
+    return $ Refraction samp ref m
+    where
+        refract nout nin norm point dir = normalize $ (scale (nout/nin) (scale (dir `dot` norm) norm) `add` dir) `sub` (scale (sqrt (1-((nout^^2)*(1-(dir `dot` norm)^^2)/(nin^^2)))) norm)
+        r1 = refract 1.0 i norm point dir
+        ray1 = Ray point r1
+        mray2 = do
+            (_, (Intersection norm point (Ray _ dir) (Transmissive i _))) <- intersectOb ray1 ob
+            return $ Ray point $ refract i 1.0 norm point dir
 
 --traceLight :: [Object] -> Intersection -> Object -> Shadow
 --traceLight scene (Intersection norm point (Ray origin dir) (Diffuse colour)) (Object light matfun) = Shadow $ scale (clamp $ lightDir `dot` norm) colour
---	where
---		lightPos = center light
---		relLightPos = lightPos `sub` point
---		distance = mag relLightPos
---		lightDir = normalize relLightPos
---		shadowRay = Ray point lightDir
---		is = filter (\(dist, _) -> dist < distance) $ catMaybes $ map (intersectOb shadowRay) scene
---		clamp fac = if fac < 0 then 0 else fac
+--  where
+--      lightPos = center light
+--      relLightPos = lightPos `sub` point
+--      distance = mag relLightPos
+--      lightDir = normalize relLightPos
+--      shadowRay = Ray point lightDir
+--      is = filter (\(dist, _) -> dist < distance) $ catMaybes $ map (intersectOb shadowRay) scene
+--      clamp fac = if fac < 0 then 0 else fac
