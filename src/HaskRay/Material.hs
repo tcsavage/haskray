ff1c10271283fd774f454d9e1782fa05e5c607d0
tcsavage <tcsavage@gmail.com>
2013-01-15 21:59:19 +0000

    Added correctColour for converting double colours to ints with correction

--- src/HaskRay/Material.hs
+++ src/HaskRay/Material.hs
@@ -19,5 +19,10 @@ addColour (Vector3 r1 g1 b1) (Vector3 r2 g2 b2) = Vector3 (r1 + r2) (g1 + g2) (b
 clampColour :: Colour -> Colour
 clampColour = fmap (\x -> if x < 0 then 0 else (if x > 1 then 1 else x))
 
+correctColour :: Double -> Int
+correctColour n = floor $ ((clamp n) ** (1/2.2)) * 255 + 0.5
+    where
+        clamp n = if n < 0 then 0 else (if n > 1 then 1 else n)
+
 -- | Defines surface properties.
 data Material = Diffuse Colour | Emissive Colour Scalar | Reflective | Transmissive Double Double deriving (Show, Read, Eq)
